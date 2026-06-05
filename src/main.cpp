f984969c0a687f1eb1250f673e6a91f5f20986f6
spencrc <145073726+spencrc@users.noreply.github.com>
2026-06-04 22:41:29 -0700

    refactor: created server class and removed server logic from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,20 @@
 #include "constants.hpp"
-#include "evented.hpp"
-#include "socket.hpp"
-#include "task.hpp"
-#include <chrono>
-#include <iostream>
 #include <liburing.h>
-
-namespace sc = std::chrono;
-
-Task<Promise> handleConnectionAsync(int clientfd, evented *ev) noexcept {
-	unsigned int exchanges = 0;
-	auto endtime = sc::steady_clock::now() + sc::seconds(KEEPALIVE_TIMEOUT);
-	std::array<char, BUFFER_LEN> read_buffer{};
-	std::array<char, BUFFER_LEN> write_buffer{};
-	__kernel_timespec ts{
-		1,
-		0,
-	};
-
-	while (++exchanges < KEEPALIVE_REQUESTS and
-		   sc::steady_clock::now() < endtime) {
-		int res = co_await RecvAwaiter(clientfd, ev, &read_buffer, &ts);
-		if (res <= 0) {
-			break;
-		}
-
-		co_await ParseAwaiter(read_buffer.data());
-
-		res = co_await WriteAwaiter(clientfd, ev, &write_buffer, &ts);
-		if (res < 0) {
-			break;
-		}
-	}
-	co_await CloseAwaiter(clientfd, ev);
-}
-
-Task<Promise> serveAsync(int sockfd, evented *ev) noexcept {
-	while (true) {
-		int clientfd = co_await AcceptAwaiter(sockfd, ev);
-		if (clientfd >= 0) {
-			handleConnectionAsync(clientfd, ev);
-		}
-	}
-}
-
-void eventLoop(evented *ev) noexcept {
-	io_uring_cqe *cqe = nullptr;
-
-	while (true) {
-		ev->wait_cqe(&cqe);
-		void *data = io_uring_cqe_get_data(cqe);
-		int res = cqe->res;
-		ev->cqe_seen(cqe);
-
-		if (!data) {
-			fprintf(stderr, "res: %d  (timeout/null)\n", res);
-			continue;
-		} else {
-			std::cout << "res: " << res << std::endl;
-		}
-
-		auto handle = std::coroutine_handle<Promise>::from_address(data);
-		handle.promise().awaiter->setRes(res);
-		handle.resume();
-	}
-}
+#include "server.hpp"
 
 int main() {
-	evented ev(QUEUE_DEPTH);
+    const coring_server::server_options opts{
+        .port = PORT,
+        .interface = INADDR_ANY,
+        .queue_depth = QUEUE_DEPTH,
+        .domain = AF_INET,
+        .type = SOCK_STREAM | SOCK_NONBLOCK,
+        .protocol = IPPROTO_TCP,
+        .max_connections = KERNEL_BACKLOG,
+    };
 
-	auto sock = Socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, IPPROTO_TCP);
-
-	constexpr unsigned int flag = 1;
-	sock.setsockopt(SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &flag, sizeof(flag));
-
-	sock.bind(PORT, INADDR_ANY);
-	sock.listen(KERNEL_BACKLOG);
-
-	auto serve = serveAsync(sock.getFd(), &ev);
-
-	eventLoop(&ev);
+    coring_server::server s(opts);
+    s.run();
 
 	return 0;
 }
---
+++ src/server.cpp
@@ -0,0 +1,89 @@
+#include "server.hpp"
+#include <chrono>
+
+using namespace coring_server;
+
+namespace sc = std::chrono;
+
+server::server(const server_options opts) :
+    port(opts.port),
+    interface(opts.interface),
+    max_connections(opts.max_connections),
+    sock(Socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, IPPROTO_TCP)),
+    ev(opts.queue_depth)
+{
+	constexpr unsigned int flag = 1;
+	sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag));
+	sock.setsockopt(SOL_SOCKET, SO_REUSEPORT, &flag, sizeof(flag));
+}
+
+void server::run() {
+    sock.bind(PORT, INADDR_ANY);
+	sock.listen(KERNEL_BACKLOG);
+
+	auto serve = serveAsync();
+
+	eventLoop();
+}
+
+Task<Promise> server::handleConnectionAsync(int clientfd) noexcept {
+	unsigned int exchanges = 0;
+	auto endtime = sc::steady_clock::now() + sc::seconds(KEEPALIVE_TIMEOUT);
+	std::array<char, BUFFER_LEN> read_buffer{};
+	std::array<char, BUFFER_LEN> write_buffer{};
+	__kernel_timespec ts{
+		1,
+		0,
+	};
+
+	while (++exchanges < KEEPALIVE_REQUESTS and
+		   sc::steady_clock::now() < endtime) {
+		int res = co_await RecvAwaiter(clientfd, &ev, &read_buffer, &ts);
+		if (res <= 0) {
+			break;
+		}
+
+		co_await ParseAwaiter(read_buffer.data());
+
+		res = co_await WriteAwaiter(clientfd, &ev, &write_buffer, &ts);
+		if (res < 0) {
+			break;
+		}
+	}
+	co_await CloseAwaiter(clientfd, &ev);
+}
+
+Task<Promise> server::serveAsync() noexcept {
+	while (true) {
+		int clientfd = co_await AcceptAwaiter(sock.getFd(), &ev);
+		if (clientfd >= 0) {
+			handleConnectionAsync(clientfd);
+		}
+	}
+}
+
+void server::eventLoop() noexcept {
+	io_uring_cqe *cqe = nullptr;
+
+	while (true) {
+		ev.wait_cqe(&cqe);
+		void *data = io_uring_cqe_get_data(cqe);
+		int res = cqe->res;
+		ev.cqe_seen(cqe);
+
+		if (!data) {
+			continue;
+		}
+
+		// if (!data) {
+		// 	fprintf(stderr, "res: %d  (timeout/null)\n", res);
+		// 	continue;
+		// } else {
+		// 	std::cout << "res: " << res << std::endl;
+		// }
+
+		auto handle = std::coroutine_handle<Promise>::from_address(data);
+		handle.promise().awaiter->setRes(res);
+		handle.resume();
+	}
+}
